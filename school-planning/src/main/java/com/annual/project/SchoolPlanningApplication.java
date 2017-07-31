d06b3741d0809d47da73329a6081fecf9256c179
Willy <ung.willybox@gmail.com>
2017-07-31 06:13:34 +0200

    Initializing repository

---
+++ school-planning/src/main/java/com/annual/project/SchoolPlanningApplication.java
@@ -0,0 +1,12 @@
+package com.annual.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolPlanningApplication.class, args);
+	}
+}
